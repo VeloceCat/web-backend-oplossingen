43beb7f1ec44dbafd23b3c46c752ab0c825a1931
VeloceCat <velocecat@gmail.com>
2017-10-05 11:00:20 +0200

    opdracht-comments

---
+++ opdracht-comments/opdracht-comments.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht comments</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+
+    <body class="web-backend-opdracht">
+        
+        <?php echo 'Vincent Van Minnebruggen'
+        
+        /*
+            Vincent Van Minnebruggen
+            vincent.vanminnebruggen@student.kdg.be
+        */
+        ?>
+
+    </body>
+</html>
