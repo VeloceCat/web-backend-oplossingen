efb1fdaa6e98ecc5a26c06891834a3e6438fc273
VeloceCat <velocecat@gmail.com>
2017-10-19 12:19:15 +0200

    opdracht-include-require

---
+++ opdracht-include-require/index.php
@@ -0,0 +1,20 @@
+<?php
+    $artikels =	array(
+        array(	'title'	=> 'PHP 8.4 gereleased',
+                'body' 	=> 'De nieuwste versie van PHP bevat uitbreidingen op oa. mind control-facades, in-browser 3d projecties en de API voor drones.',
+                'tags' 	=> 'Kernwoorden: new, PHP 8.4',
+        ),
+        array(	'title'	=> 'Zymphovel framework',
+                'body' 	=> 'Wordt Zymphovel het nieuwste PHP-framework dat de wereld in een sneltempo zal veroveren?',
+                'tags' 	=> 'Kernwoorden: frameworks',
+        ),
+        array(	'title'	=> 'Rasmus Lerdorf vermist',
+                'body' 	=> 'De geestelijke vader van PHP Rasmus Lerdorf is sinds vorige week vermist nadat hij met zijn privé-jet richting Ibiza vertrok.',
+                'tags' 	=> 'Kernwoorden: bizar, rasmus lerdorf, feestneus',
+        )
+    );
+
+    include 'view/header-partial.html';
+    include 'view/body-partial.html'; 
+    include 'view/footer-partial.html';
+?>
