88fceb35f826b7f7615a9cd09c846491b7dd23eb
VeloceCat <velocecat@gmail.com>
2017-12-18 13:44:27 +0100

    opdracht-image-manipulation-thumb

---
+++ opdracht-image-manipulation-thumb/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP oefening 30 - image manipulation</title>
+	<link rel="stylesheet" type="text/css" href="../../_assets/css/global.css">
+</head>
+<body class="web-backend-voorbeeld">
+
+	<form action="cropped.php" method="POST" enctype="multipart/form-data">
+		
+		<label for="image">image</label>
+		<input type="file" name="image" id="image">
+
+		<input type="submit" name="submit">
+		
+	</form>
+
+	</body>
+</html>
